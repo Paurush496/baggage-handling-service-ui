4853598fff54b25880be51437c7bc8739280f87b
Paurush Choudhary <paurushchoudhary@Paurushs-MacBook-Pro.local>
2024-04-18 14:58:01 +0530

    Heroku deployment

--- server.js
+++ server.js
@@ -1,8 +1,8 @@
 const express = require('express');
 const path = require('path');
 const app = express();
-app.use(express.static(__dirname + '/dist/baggage-handler-service-ui'));
+app.use(express.static(__dirname + '/dist/baggage-handler-service-ui/browser'));
 app.get('/*', function(req,res) {
 res.sendFile(path.join(__dirname+
-'/dist/baggage-handler-service-ui/index.html'));});
+'/dist/baggage-handler-service-ui/browser/index.html'));});
 app.listen(process.env.PORT || 8080);
